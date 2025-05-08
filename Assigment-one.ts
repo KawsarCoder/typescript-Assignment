ab386ab1e51256176c1db4544be3e62451892dcb
KawsarCoder <md.kawsar.developer@gmail.com>
2025-05-08 20:56:17 +0600

    problem seven done

--- Assigment-one.ts
+++ Assigment-one.ts
@@ -66,5 +66,23 @@
     }
   }
 
+  // Problem seven
+  enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+  }
+
+  function getDayType(day: Day): string {
+    if (day === Day.Saturday || day === Day.Sunday) {
+      return "Weekend";
+    }
+    return "Weekday";
+  }
+
   //
 }
